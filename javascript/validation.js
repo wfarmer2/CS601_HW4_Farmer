8426d2e8025f40c90ef3f7f699f1a7d349573487
Will Farmer <wfarmer2@bu.edu>
2022-11-27 15:02:03 -0700

    Update styling and validation checks

--- javascript/validation.js
+++ javascript/validation.js
@@ -9,13 +9,13 @@ function validateFirstName() {
     let firstName = document.getElementById("firstName");
 
     if (firstName.value.length <= 2) {
-        alert("Invalid name! First name must contain two (2) or more characters.");
+        document.getElementById("firstNamePrompt").innerHTML = ("Invalid name! First name must contain two (2) or more characters.");
         firstName.focus();
         return false;
     }
 
     if (!(firstName.value.match(letterPattern))) {
-        alert("Invalid name! First name must contain only alphabetic characters.")
+        document.getElementById("firstNamePrompt").innerHTML = ("Invalid name! First name must contain only alphabetic characters.")
         firstName.focus();
         return false;
     }
@@ -28,13 +28,13 @@ function validateLastName() {
     let lastName = document.getElementById("lastName");
 
     if (lastName.value.length <= 2) {
-        alert("Invalid name! Last name must contain two (2) or more characters.");
+        document.getElementById("lastNamePrompt").innerHTML = ("Invalid name! Last name must contain two (2) or more characters.");
         lastName.focus();
         return false;
     }
 
     if (!(lastName.value.match(letterPattern))) {
-        alert("Invalid name! Last name must contain only alphabetic characters.")
+        document.getElementById("lastNamePrompt").innerHTML = ("Invalid name! Last name must contain only alphabetic characters.")
         lastName.focus();
         return false;
     }
@@ -49,7 +49,7 @@ function validateFacilitator() {
     facilitator = facilitator.value.toUpperCase();
     
     if (!facilitatorList.includes(facilitator)) {
-        alert("Invalid facilitator name! Please type a current facilitator name.")
+        document.getElementById("facilitatorPrompt").innerHTML = ("Invalid facilitator name! Please type a current facilitator name.")
         facilitator.focus();
         return false;
     }
