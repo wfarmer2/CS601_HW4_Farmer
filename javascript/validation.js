a4f7b0f28685ae71d0bbd46a64caee37478325bf
Will Farmer <wfarmer2@bu.edu>
2022-11-27 23:08:27 -0700

    Updated styling, validation, and format

--- javascript/validation.js
+++ javascript/validation.js
@@ -1,21 +1,32 @@
 
 const letterPattern = /^[A-Za-z]+$/;
 
-function validateForm() {
-    return (validateFirstName() && validateLastName() && validateFacilitator());
+function validateForm(event) {
+    event.preventDefault();
+    
+    let validFirstName = validateFirstName();
+    let validLastName = validateLastName();
+    let validFacilitator = validateFacilitator();
+
+    if (!validFirstName || !validLastName || !validFacilitator) {
+        return alert("Please correct form errors before submission.");
+    }
+
+    event.target.submit();
+
 }
 
 function validateFirstName() {
     let firstName = document.getElementById("firstName");
 
-    if (firstName.value.length <= 2) {
-        document.getElementById("firstNamePrompt").innerHTML = ("Invalid name! First name must contain two (2) or more characters.");
+    if (firstName.value.length <= 2 || firstName.value == null || firstName.value == "") {
+        document.getElementById("firstNamePrompt").innerHTML = ("<span style='color:red'> Invalid name! First name must contain two (2) or more characters.</span>");
         firstName.focus();
         return false;
     }
 
     if (!(firstName.value.match(letterPattern))) {
-        document.getElementById("firstNamePrompt").innerHTML = ("Invalid name! First name must contain only alphabetic characters.")
+        document.getElementById("firstNamePrompt").innerHTML = ("<span style='color:red'> Invalid name! First name must contain only alphabetic characters.</span>")
         firstName.focus();
         return false;
     }
@@ -27,14 +38,14 @@ function validateFirstName() {
 function validateLastName() {
     let lastName = document.getElementById("lastName");
 
-    if (lastName.value.length <= 2) {
-        document.getElementById("lastNamePrompt").innerHTML = ("Invalid name! Last name must contain two (2) or more characters.");
+    if (lastName.value.length <= 2 || lastName.value == null || lastName.value == "") {
+        document.getElementById("lastNamePrompt").innerHTML = ("<span style='color:red'> Invalid name! Last name must contain two (2) or more characters.</span>");
         lastName.focus();
         return false;
     }
 
     if (!(lastName.value.match(letterPattern))) {
-        document.getElementById("lastNamePrompt").innerHTML = ("Invalid name! Last name must contain only alphabetic characters.")
+        document.getElementById("lastNamePrompt").innerHTML = ("<span style='color:red'> Invalid name! Last name must contain only alphabetic characters.</span>")
         lastName.focus();
         return false;
     }
@@ -45,11 +56,9 @@ function validateLastName() {
 function validateFacilitator() {
     let facilitator = document.getElementById("facilitator")
     const facilitatorList = ["JEN", "BEHDAD", "CHRIS", "CHRISTIAN", "JOSH"]
-
-    facilitator = facilitator.value.toUpperCase();
     
-    if (!facilitatorList.includes(facilitator)) {
-        document.getElementById("facilitatorPrompt").innerHTML = ("Invalid facilitator name! Please type a current facilitator name.")
+    if (!facilitatorList.includes(facilitator.value.toUpperCase())) {
+        document.getElementById("facilitatorPrompt").innerHTML = ("<span style='color:red'> Invalid facilitator name! Please type a current facilitator name: Jen, Behdad, Chris, Christian, or Josh.</span>")
         facilitator.focus();
         return false;
     }
